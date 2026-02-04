08971a42bbbce617c948efbbf398729e2cba590b
keitaroi446 <149392536+keitaroi446@users.noreply.github.com>
2026-02-04 17:08:33 +0900

    修正

--- stm32/nucleo_f446re/lib/c620_can/c620_can.hpp
+++ stm32/nucleo_f446re/lib/c620_can/c620_can.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <cstdint>
 
 class C620CAN
@@ -15,7 +16,7 @@ public:
     uint32_t getRxCount() const;
 
 private:
-    static constexpr float LIMIT_CURRENT_AMP = 6.0f;    // ユーザー定義の電流制限[A]
+    static constexpr float LIMIT_CURRENT_AMP = 6.0f;   // ユーザー定義の電流制限[A]
     static constexpr float MAX_CURRENT_AMP = 20.0f;     // 最大電流[A]
     static constexpr int16_t MAX_CURRENT_RAW = 16384;   // 最大電流(16bit)
     static constexpr uint16_t MAX_ANGLE_RAW = 8191;     // 最大角度(16bit)
