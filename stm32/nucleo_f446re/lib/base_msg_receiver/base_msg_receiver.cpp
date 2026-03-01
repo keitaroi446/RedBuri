50aa4bd8973800e8818ae7568a3766a48d78652c
keitaroi446 <149392536+keitaroi446@users.noreply.github.com>
2026-03-01 14:12:04 +0900

    update BaseMsgReciver to parse msgs

--- stm32/nucleo_f446re/lib/base_msg_receiver/base_msg_receiver.cpp
+++ stm32/nucleo_f446re/lib/base_msg_receiver/base_msg_receiver.cpp
@@ -1,32 +1,82 @@
-#include <cstdio>
-
 #include "base_msg_receiver.hpp"
 
-bool BaseMsgReceiver::readMsg(const char *line)
+#include <cstdio>
+#include <cstring>
+
+BaseMsgReceiver::BaseMsgReceiver(UART_HandleTypeDef &huart)
+    : huart_(huart)
+{}
+
+void BaseMsgReceiver::init()
 {
-    char msg_type{};
-    float steer{};
-    float motor_f{};
-    float motor_r{};
-    float motor_l{};
+    HAL_UART_Receive_IT(&huart_, &msg_, 1);
+}
+
+void BaseMsgReceiver::readMsg()
+{
+    if (msg_ == '\n')
+    {
+        rx_buf_[rx_len_] = '\0';
+
+        char msg_type{};
+        float steer{};
+        float motor_f{};
+        float motor_r{};
+        float motor_l{};
+
+        const int parsed = std::sscanf(
+            rx_buf_,
+            "%c,%f,%f,%f,%f",
+            &msg_type,
+            &steer,
+            &motor_f,
+            &motor_r,
+            &motor_l
+        );
+
+        if (parsed == 5 && msg_type == 'B')
+        {
+            steer_deg_ = steer;
+            motor_f_rpm_ = motor_f;
+            motor_r_rpm_ = motor_r;
+            motor_l_rpm_ = motor_l;
+        }
+
+        rx_len_ = 0;
+        std::memset(rx_buf_, 0, sizeof(rx_buf_));
+    }
+    else
+    {
+        if (rx_len_ < RX_BUF_SIZE - 1)
+        {
+            rx_buf_[rx_len_++] = static_cast<char>(msg_);
+        }
+        else
+        {
+            rx_len_ = 0;
+            std::memset(rx_buf_, 0, sizeof(rx_buf_));
+        }
+    }
+
+    HAL_UART_Receive_IT(&huart_, &msg_, 1);
 }
 
 float BaseMsgReceiver::getSteerDeg() const
 {
-    return steer_deg;
+    return steer_deg_;
 }
 
 float BaseMsgReceiver::getMotorFRpm() const
 {
-    return motor_f_rpm;
+    return motor_f_rpm_;
 }
 
 float BaseMsgReceiver::getMotorRRpm() const
 {
-    return motor_r_rpm;
+    return motor_r_rpm_;
 }
 
 float BaseMsgReceiver::getMotorLRpm() const
 {
-    return motor_l_rpm;
+    return motor_l_rpm_;
 }
--- stm32/nucleo_f446re/lib/base_msg_receiver/base_msg_receiver.hpp
+++ stm32/nucleo_f446re/lib/base_msg_receiver/base_msg_receiver.hpp
@@ -1,20 +1,29 @@
 #pragma once
 
 #include <cstdint>
+#include <cstddef>
+#include "usart.h"
 
 class BaseMsgReceiver
 {
 public:
-    bool readMsg(const char *line);
-
+    explicit BaseMsgReceiver(UART_HandleTypeDef &huart);
+    void init();
+    void readMsg();
     float getSteerDeg() const;
     float getMotorFRpm() const;
     float getMotorRRpm() const;
     float getMotorLRpm() const;
 
 private:
-    float steer_deg{};
-    float motor_f_rpm{};
-    float motor_r_rpm{};
-    float motor_l_rpm{};
+    static constexpr size_t RX_BUF_SIZE = 64;
+
+    UART_HandleTypeDef &huart_;
+    uint8_t msg_{};
+    char rx_buf_[RX_BUF_SIZE]{};
+    size_t rx_len_{};
+    float steer_deg_{};
+    float motor_f_rpm_{};
+    float motor_r_rpm_{};
+    float motor_l_rpm_{};
 };
